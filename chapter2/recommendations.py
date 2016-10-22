fe7dafa870662d632780060ec23de8f9420cfc62
aoi-lucario <petersbw123@gmail.com>
2016-10-22 17:23:14 +0000

    edit chapter2/recommendations.py

--- chapter2/recommendations.py
+++ chapter2/recommendations.py
@@ -1,29 +1,21 @@
 from math import sqrt
 
-critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
- 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
- 'The Night Listener': 3.0},
-'Gene Seymour': {'Lady in the Water': 3.0,'Snakes on a Plane': 3.5,
- 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
- 'You, Me and Dupree': 3.5},
-'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
- 'Superman Returns': 3.5, 'The Night Listener': 4.0},
-'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
- 'The Night Listener': 4.5, 'Superman Returns': 4.0,
- 'You, Me and Dupree': 2.5},
-'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
- 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
- 'You, Me and Dupree': 2.0},
-'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
- 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
-'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0,'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 3.5},
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'The Night Listener': 4.5, 'Superman Returns': 4.0, 'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 2.0},
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0}}
 
-def sim_dinstance(prefs, person1, person2):
+
+def sim_distance(prefs, person1, person2):
     si = {}
     for item in prefs[person1]:
         if item in prefs[person2]:
             si[item] = 1
-    if len(si)==0: return(0)
+    if len(si) == 0:
+        return 0
 
     sum_of_squares = sum((prefs[person1][item] - prefs[person2][item])**2 for item in si)
     return 1/(1+sqrt(sum_of_squares))
@@ -35,7 +27,8 @@ def sim_pearson(prefs, person1, person2):
         if item in prefs[person2]:
             si[item] = 1
     n = len(si)
-    if n == 0: return(0)
+    if n == 0:
+        return 0
 
     sum1 = sum(prefs[person1][x] for x in si)
     sum2 = sum(prefs[person2][x] for x in si)
@@ -47,8 +40,74 @@ def sim_pearson(prefs, person1, person2):
 
     num = sump - (sum1*sum2/n)
     den = sqrt((sum1sq-sum1**2/n)*(sum2sq-sum2**2/n))
-    if den == 0: return 0
+    if den == 0:
+        return 0
 
     r = num/den
     return r
-    
+
+
+def topMatches(prefs, person, n=5, similarity=sim_pearson):
+    scores = [(similarity(prefs, person, other), other) for other in prefs if other != person]
+    scores.sort()
+    scores.reverse()
+    return scores[:n]
+
+
+def getRecommendations(prefs, person, similarity=sim_pearson):
+    totals={}
+    simSums={}
+    for other in prefs:
+        if other == person: continue
+        sim = similarity(prefs, person, other)
+        if sim <= 0: continue
+
+        for item in prefs[other]:
+            if item not in prefs[person] or prefs[person][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item]*sim
+
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+    rankings = [(totals[item]/simSums[item], item) for item in totals]
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+
+def transformPrefs(prefs):
+    result={}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item,{})
+            result[item][person] = prefs[person][item]
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
