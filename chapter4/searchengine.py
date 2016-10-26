c0ed1a2d592081d586c5d42f17a774cc36754766
aoi-luacrio <petersbw123@gmail.com>
2016-10-26 16:13:30 +0900

    edit chapter4/searchengine.py

--- chapter4/searchengine.py
+++ chapter4/searchengine.py
@@ -1,4 +1,5 @@
 import urllib3
+import re
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from sqlite3 import dbapi2 as sqlite
@@ -16,18 +17,50 @@ class crawler:
         self.con.commit()
 
     def getentryid(self, table, field, value, createnew = True):
-        return None
+        cur = self.con.execute("select rowid from %s where %s = '%s'" % (table, field, value))
+        res = cur.fetchone()
+        if res == None:
+            cur = self.con.execute("insert into %s (%s) values ('%s')" % (table, field, value))
+            return cur.lastrowid
+        else:
+            return res[0]
 
     def addtoindex(self, url, soup):
+        if self.isindexed(url):return
         print('Indexing %s' % url)
 
+        text = self.gettextonly(soup)
+        words = self.separatewords(text)
+
+        urlid = self.getentryid('urllist', 'url', url)
+
+        for i in range(len(words)):
+            word = words[i]
+            if word in ignorewords: continue
+            wordid = self.getentryid('wordlist', 'word', word)
+            self.con.execute("insert into wordlocation(urlid, wordid, location) values(%d, %d, %d)" % (urlid, wordid, i))
+
     def gettextonly(self, soup):
-        return None
+        v = soup.string
+        if v == None:
+            c = soup.contents
+            resulttext = ''
+            for t in c:
+                subtext = self.gettextonly(t)
+                resulttext += subtext + '\n'
+            return resulttext
+        else:
+            return v.strip()
 
     def separatewords(self, text):
-        return None
+        splitter = re.compile('\\W*')
+        return [s.lower() for s in splitter.split(text) if s != '']
 
     def isindexed(self, url):
+        u = self.con.execute("select rowid from urllist where url='%s'" % url).fetchone()
+        if u != None:
+            v = self.con.execute('select * from wordlocation where urlid=%d' % u[0]).fetchone()
+            if v != None: return True
         return False
 
     def addlinkref(self, urlFrom, urlTo, linkText):
@@ -59,4 +92,14 @@ class crawler:
             pages = newpages
 
     def createindextables(self):
-        pass
+        self.con.execute('create table urllist(url)')
+        self.con.execute('create table wordlist(word)')
+        self.con.execute('create table wordlocation(urlid, wordid, location)')
+        self.con.execute('create table link(fromid integer, toid integer)')
+        self.con.execute('create table linkwords(wordid, linkid)')
+        self.con.execute('create index wordidx on wordlist(word)')
+        self.con.execute('create index urlidx on urllist(url)')
+        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index urltoidx on link(toid)')
+        self.con.execute('create index urlfromidx on link(fromid)')
+        self.dbcommit()
